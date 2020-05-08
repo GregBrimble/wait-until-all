256f823b4a04165dd35a32c89a0fd5f389820956
Greg Brimble <developer@gregbrimble.com>
2020-05-08 16:00:49 +0100

    v1

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,45 @@
-import { gretter } from ".";
+import "cross-fetch/polyfill";
+import { registerWaitUntil, executeWaitUntil } from "./index";
 
-describe("gretter", () => {
-  it("works with a name", () => {
-    expect(gretter("John")).toEqual("Hello, John!");
+interface Event {
+  respondWith: (response: Promise<Response> | Response) => void;
+  waitUntil: (promise: Promise<any>) => void;
+}
+
+describe("wait-until-all", () => {
+  let event: Event;
+  let cleanupPromise: Promise<any>;
+
+  beforeEach(() => {
+    const promises = [];
+
+    event = {
+      respondWith: () => {},
+      waitUntil: (promise: Promise<any>) => {
+        cleanupPromise = promise;
+      },
+    };
   });
-  it("works without a name", () => {
-    expect(gretter()).toEqual("Hello, World!");
+
+  it("registers and executes multiple Promises", async () => {
+    let promiseExecuted = false;
+
+    const handler = (event: Event) => {
+      registerWaitUntil(
+        Promise.resolve("world").then(() => {
+          promiseExecuted = true;
+        })
+      );
+      return new Response("Hello, world!", {
+        headers: { "Content-Type": "text/plain" },
+      });
+    };
+
+    event.respondWith(handler(event));
+    executeWaitUntil(event);
+
+    await cleanupPromise;
+
+    expect(promiseExecuted).toBeTruthy();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export const gretter = (name?: string) => `Hello, ${name || "World"}!`;
+const WAIT_UNTILS = [];
+
+type Event = {
+  waitUntil: (promise: Promise<any>) => any;
+};
+
+export const registerWaitUntil = (promise: Promise<any>) =>
+  WAIT_UNTILS.push(promise);
+
+export const executeWaitUntil = (event: Event) =>
+  event.waitUntil(Promise.all(WAIT_UNTILS));
