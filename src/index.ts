08b7c3f52c332a5df7f99a127156020b7b4ddce1
Greg Brimble <developer@gregbrimble.com>
2020-05-08 17:37:22 +0100

    Add typing information

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ type Event = {
   waitUntil: (promise: Promise<any>) => any;
 };
 
-export const registerWaitUntil = (promise: Promise<any>) =>
+export const registerWaitUntil = (promise: Promise<any>) => {
   WAIT_UNTILS.push(promise);
+};
 
-export const executeWaitUntil = (event: Event) =>
+export const executeWaitUntil = (event: Event) => {
   event.waitUntil(Promise.all(WAIT_UNTILS));
+};
